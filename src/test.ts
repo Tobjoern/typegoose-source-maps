0372a3de7baaa5fb8997e6925700f8dd4f1d3df6
tobjoern <Tobi11111@gmx.de>
2021-10-01 21:21:14 +0200

    Added: Source maps

--- src/test.ts
+++ src/test.ts
@@ -1,26 +1,35 @@
 // NodeJS: 16.8.0
 // MongoDB: 4.2-bionic (Docker)
+import 'source-map-support/register';
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@8.3.0
 import * as mongoose from 'mongoose'; // mongoose@5.13.9
 
 class User {
-  @prop()
-  public username?: string;
+  @prop({ required: true })
+  public name!: string;
 }
 
 const UserModel = getModelForClass(User);
 
+async function doStuff() {
+  const doc = new UserModel();
+
+  await doc.save();
+}
+
 (async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, {
+  await mongoose.connect(`mongodb://localhost:27017/experiments-3`, {
     useNewUrlParser: true,
-    dbName: 'verifyMASTER',
     useCreateIndex: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
   });
 
-  const doc = new UserModel({ username: 'user1' });
-
-  console.log(doc);
+  try {
+    await doStuff();
+  } catch (e) {
+    console.error(e);
+  }
 
   await mongoose.disconnect();
 })();
